b761065190fd105d3a0d9fd799b47dddadb60fc7
puneetvashisht@gmail.com <puneetvashsiht@gmail.com>
2019-11-28 21:37:06 +0530

    ribbon load balancing & naming server

--- its-currency-exchange-service/src/main/java/com/itc/itscurrencyexchangeservice/ItsCurrencyExchangeServiceApplication.java
+++ its-currency-exchange-service/src/main/java/com/itc/itscurrencyexchangeservice/ItsCurrencyExchangeServiceApplication.java
@@ -2,8 +2,10 @@ package com.itc.itscurrencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ItsCurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
